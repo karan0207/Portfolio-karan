2d0db66ae05d784290348e95974ec526677bd08c
Karan0207 <minidox0207@gmail.com>
2023-12-17 22:10:41 +0530

    mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <header className='header'>
-      <NavLink to='/' className='w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md'>
+      <NavLink to='/Portfolio-karan' className='w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md'>
        
         <p  className='blue-gradient_text'>KSC</p>
       </NavLink>
