e1153e597511c53c68540ce542e28c2f010f028e
Karan0207 <minidox0207@gmail.com>
2023-12-17 11:16:08 +0530

    finaldeploy

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { About, Contact, Home, Projects } from "./pages";
 const App = () => {
   return (
     <main className='bg-slate-300/20 h-full'>
-      <Router>
+      <Router basename="/Portfolio-karan">
         <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
